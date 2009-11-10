49c9421df36a058ac6d128c57443deb0d61c1704
Juan de Frías <juafrlo@gmail.com>
2009-11-10 08:05:47 +0100

    Using camelize to work with models named with '_'

--- lib/identity.rb
+++ lib/identity.rb
@@ -6,12 +6,12 @@ module ActsAsFavorite
 
         def method_missing( method_sym, *args )
           if !method_sym.to_s.scan(Regexp.new("^favorite_(\\w+)")).blank?
-            favorite_class = ($1).singularize.capitalize.constantize
+            favorite_class = ($1).singularize.camelize.constantize
             favorite_class.find(:all, :include => :favorites,
                                 :conditions => ['favorites.user_id = ? AND favorites.favorable_type = ?', 
                                                 id, favorite_class.to_s ] )
           elsif !method_sym.to_s.scan(Regexp.new("^has_favorite_(\\w+)\\?")).blank?
-            favorite_class = ($1).singularize.capitalize.constantize
+            favorite_class = ($1).singularize.camelize.constantize
             Favorite.count( :include => :user, :conditions => [ 'favorites.user_id = ? AND favorites.favorable_type = ?', 
                                                                 id, favorite_class.to_s ] ) != 0
           else
